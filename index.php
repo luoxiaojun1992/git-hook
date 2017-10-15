de9ca43003aa5b54faf16797e4f438f62b8dca72
luoxiaojun1992 <luoxiaojun1992@sina.cn>
2017-10-15 23:10:39 +0800

    init commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+define('PROD_BRANCH', 'master');
+
+$secret = '3JsqJSHVTsx1uKqy';
+
+$jsonData = file_get_contents('php://input');
+
+if ($_SERVER['HTTP_X_HUB_SIGNATURE'] != ('sha1=' . hash_hmac('sha1', $jsonData, $secret))) {
+	exit(1);
+}
+
+$data = json_decode($jsonData, true);
+if ($data) {
+	$ref = $data['ref'];
+	$refArr = explode('/', $ref);
+	$branch = $refArr[2];
+	if ($branch == PROD_BRANCH) {
+		system('./build.sh');
+	}
+}
