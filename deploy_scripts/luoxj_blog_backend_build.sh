f9fca39f38ac84264621aea3f9754091be7fd2a2
Gogs <gogitservice@gmail.com>
2017-10-28 01:33:42 +0800

    update deploy

--- deploy_scripts/luoxj_blog_backend_build.sh
+++ deploy_scripts/luoxj_blog_backend_build.sh
@@ -8,3 +8,11 @@ else
     echo 'Build failed'
 fi
 
+ssh root@47.96.252.49 -l root 'cd /home/www/api.fourleaver.com && git pull && ./lb migrate/up'
+
+if [ $? -eq 0 ]; then
+    echo 'Build succeed'
+else
+    echo 'Build failed'
+fi
+
