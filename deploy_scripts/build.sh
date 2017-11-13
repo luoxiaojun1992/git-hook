475a009d3ed94f8fbe577baa2e0ec13c7397ea2b
Gogs <gogitservice@gmail.com>
2017-11-13 22:54:34 +0800

    update

--- deploy_scripts/build.sh
+++ deploy_scripts/build.sh
@@ -8,3 +8,10 @@ else
     echo 'Build failed'
 fi
 
+ssh root@47.96.252.49 -l root 'releaseId=luoxj-blog-release/luoxj-blog-release-`date +%Y%m%d%H%M%s` && cp -r ~/luoxj-blog ~/$releaseId && cd ~/$releaseId && git pull && npm install && npm run build && rm -rf ~/luoxj-blog && cp -r ~/$releaseId ~/luoxj-blog && supervisorctl restart luoxj-blog'
+
+if [ $? -eq 0 ]; then
+    echo 'Build succeed'
+else
+    echo 'Build failed'
+fi
