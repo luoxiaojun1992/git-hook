42a7a2e271144db35b176dc6aadb8fd263291e44
Gogs <gogitservice@gmail.com>
2018-01-02 15:45:26 +0800

    refactor

--- deploy_scripts/build.sh
+++ deploy_scripts/build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-ssh root@10.28.231.170 -l root 'releaseId=luoxj-blog-release/luoxj-blog-release-`date +%Y%m%d%H%M%s` && cp -r ~/luoxj-blog ~/$releaseId && cd ~/$releaseId && git pull && npm install && npm run build && rm -rf ~/luoxj-blog && cp -r ~/$releaseId ~/luoxj-blog && supervisorctl restart luoxj-blog && rm -rf ~/luoxj-blog-release/prod && cp -r ~/$releaseId ~/luoxj-blog-release/prod'
+ssh root@10.28.231.170 -l root 'releaseId=luoxj-blog-release/luoxj-blog-release-`date +%Y%m%d%H%M%s` && cp -r ~/luoxj-blog-release/prod ~/$releaseId && cd ~/$releaseId && git pull && npm install && npm run build && rm -rf ~/luoxj-blog && cp -r ~/$releaseId ~/luoxj-blog && supervisorctl restart luoxj-blog && rm -rf ~/luoxj-blog-release/prod && cp -r ~/$releaseId ~/luoxj-blog-release/prod'
 
 if [ $? -eq 0 ]; then
     echo 'Build succeed'
