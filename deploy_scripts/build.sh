4bd2b388eb0af4d60a7a9d41a151cd459a785b1d
Gogs <gogitservice@gmail.com>
2018-01-02 15:52:26 +0800

    refactor

--- deploy_scripts/build.sh
+++ deploy_scripts/build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-ssh root@10.28.231.170 -l root 'releaseId=luoxj-blog-release/luoxj-blog-release-`date +%Y%m%d%H%M%s` && cp -r ~/luoxj-blog-release/prod ~/$releaseId && cd ~/$releaseId && git pull && npm install && npm run build && rm -rf ~/luoxj-blog && cp -r ~/$releaseId ~/luoxj-blog && supervisorctl restart luoxj-blog && rm -rf ~/luoxj-blog-release/prod && cp -r ~/$releaseId ~/luoxj-blog-release/prod'
+ssh root@10.28.231.170 -l root 'releaseId=luoxj-blog-release/luoxj-blog-release-`date +%Y%m%d%H%M%s` && cp -r ~/luoxj-blog-release/prod ~/$releaseId && cd ~/$releaseId && git pull && npm install && npm run build && cp -r ~/$releaseId ~/luoxj-blog-pre && rm -rf ~/luoxj-blog && mv ~/luoxj-blog-pre ~/luoxj-blog && supervisorctl restart luoxj-blog && rm -rf ~/luoxj-blog-release/prod && cp -r ~/$releaseId ~/luoxj-blog-release/prod'
 
 if [ $? -eq 0 ]; then
     echo 'Build succeed'
@@ -9,7 +9,7 @@ else
     exit 1
 fi
 
-ssh root@10.81.74.11 -l root 'releaseId=luoxj-blog-release/prod && rm -rf ~/luoxj-blog && cp -r ~/$releaseId ~/luoxj-blog && supervisorctl restart luoxj-blog'
+ssh root@10.81.74.11 -l root 'releaseId=luoxj-blog-release/prod && cp -r ~/$releaseId ~/luoxj-blog-pre && rm -rf ~/luoxj-blog && mv ~/luoxj-blog-pre ~/luoxj-blog && supervisorctl restart luoxj-blog'
 
 if [ $? -eq 0 ]; then
     echo 'Build succeed'
