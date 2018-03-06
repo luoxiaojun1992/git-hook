7301aeb29e0a34e461b7e4de46fd3896404f1a14
罗晓俊 <gogitservice@gmail.com>
2018-03-06 09:28:08 +0800

    bugfix

--- deploy_scripts/luoxj_blog_backend_build.sh
+++ deploy_scripts/luoxj_blog_backend_build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-ssh root@10.28.231.170 -l root 'cd /home/www/api.fourleaver.com && git pull && ./lb migrate/up'
+ssh root@10.28.231.170 -l root 'cd /home/www/api.fourleaver.com && git pull && ./lb migrate/up && composer install -vvv'
 
 if [ $? -eq 0 ]; then
     echo 'Build succeed'
@@ -8,7 +8,7 @@ else
     echo 'Build failed'
 fi
 
-ssh root@10.81.74.11 -l root 'cd /home/www/api.fourleaver.com && git pull && ./lb migrate/up'
+ssh root@10.81.74.11 -l root 'cd /home/www/api.fourleaver.com && git pull && ./lb migrate/up && composer install -vvv'
 
 if [ $? -eq 0 ]; then
     echo 'Build succeed'
--- deploy_scripts/services_build.sh
+++ deploy_scripts/services_build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-ssh root@10.28.231.170 -l root 'cd /home/www/services && git pull && ./lb migrate/up'
+ssh root@10.28.231.170 -l root 'cd /home/www/services && git pull'
 
 if [ $? -eq 0 ]; then
     echo 'Build succeed'
@@ -8,7 +8,7 @@ else
     echo 'Build failed'
 fi
 
-ssh root@10.81.74.11 -l root 'cd /home/www/services && git pull && ./lb migrate/up'
+ssh root@10.81.74.11 -l root 'cd /home/www/services && git pull'
 
 if [ $? -eq 0 ]; then
     echo 'Build succeed'
